17b68952308b9ae7972782d324c04a4234f36898
Makc <mks59k@gmail.com>
2018-12-27 23:46:47 +0200

    Реализация метода получения локального Ip адреса.

--- Chat/Chat.cs
+++ Chat/Chat.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +32,59 @@ namespace Chat
             }
 
             // TODO закрыть сокет !!!
+        }
+
+        internal void Listening()
+        {
+            Task.Factory.StartNew(Listen);
+        }
+
+        private void Listen()
+        {
+            try
+            {
+                MessageBox.Show(GetLocalIpAddress());
+                //MessageBox.Show()
+
+                // Прослушиваем по адресу (наш локальный адрес).
+                //IPEndPoint localIp 
+                //    = new IPEndPoint(
+                //        IPAddress)
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+
+        private string GetLocalIpAddress()
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            throw new Exception("No network adapters " +
+                "with an IPv4 address in the system!");
+        }
+
+        internal void Close()
+        {
+            if (this.socket != null)
+            {
+                this.socket.Shutdown(SocketShutdown.Both);
+                this.socket.Close();
+                this.socket = null;
+            }
         }
     }
 }
--- Chat/MainForm.cs
+++ Chat/MainForm.cs
@@ -12,9 +12,34 @@ namespace Chat
 {
     public partial class MainForm : Form
     {
+        private Chat chat = null;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.Load += MainForm_Load;
+            this.FormClosing += MainForm_FormClosing;
+        }
+
+        
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            chat = new Chat();
+            chat.Listening();
+
+            // Down -> method registrationLogin
+
+            // TODO: Спросить Логин
+            // "Ваш логин: "
+            this.Text = "TODO: мой логин";
+        }
+
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            chat.Close();
         }
     }
 }
